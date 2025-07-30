3058ab020c2894a54a9b4eb22be1f09f96585dbb
rferpae569 <rferpae569@g.educaand.es>
2025-07-30 20:37:42 +0200

    Updates

---
+++ icons/ProyectosWeb.js
@@ -0,0 +1,138 @@
+export const webProjects = [
+  {
+    title: "AlienGotchi",
+    description: "En este proyecto tendremos que cuidar a nuestro Alienígena...",
+    tech: [
+      { name: "HTML5", icon: "/icons/html5.png" },
+      { name: "CSS", icon: "/icons/css.png" },
+      { name: "JavaScript", icon: "/icons/js.png" },
+      { name: "Git", icon: "/icons/git.png" }
+    ],
+    image: "/logo.png",
+    repo: "https://github.com/tuusuario/portafolio",
+    demo: "https://tuusuario.github.io/portafolio",
+    date: "mar. 2025 - abr. 2025"
+  },
+  {
+    title: "Casino",
+    description: "En este proyecto veremos una web realizada con Angular...",
+    tech: [
+      { name: "Angular", icon: "/icons/angular.png" },
+      { name: "PhpMyAdmin", icon: "/icons/phpmyadmin.png" },
+      { name: "PHP", icon: "/icons/php.png" },
+      { name: "Git", icon: "/icons/git.png" },
+      { name: "HTML5", icon: "/icons/html5.png" },
+      { name: "CSS", icon: "/icons/css.png" },
+      { name: "MySQL", icon: "/icons/mysql.png" },
+      { name: "TypeScript", icon: "/icons/typescript.png" }
+    ],
+    image: "/logo.png",
+    repo: "https://github.com/tuusuario/portafolio",
+    demo: "https://tuusuario.github.io/portafolio",
+    date: "dic. 2024 - ene. 2025"
+  },
+  {
+    title: "EscritorioVentanas",
+    description: "En este proyecto, veremos un escritorio en el que se mostraran seis widget...",
+    tech: [
+      { name: "React", icon: "/icons/react.png" },
+      { name: "Git", icon: "/icons/git.png" },
+      { name: "HTML5", icon: "/icons/html5.png" },
+      { name: "CSS", icon: "/icons/css.png" }
+    ],
+    image: "/logo.png",
+    repo: "https://github.com/tuusuario/portafolio",
+    demo: "https://tuusuario.github.io/portafolio",
+    date: "nov. 2024"
+  },
+  {
+    title: "Pokedex",
+    description: "En este proyecto encontraremos una Pokedex...",
+    tech: [
+      { name: "HTML5", icon: "/icons/html5.png" },
+      { name: "CSS", icon: "/icons/css.png" },
+      { name: "JavaScript", icon: "/icons/js.png" },
+      { name: "Git", icon: "/icons/git.png" }
+    ],
+    image: "/logo.png",
+    repo: "https://github.com/tuusuario/portafolio",
+    demo: "https://tuusuario.github.io/portafolio",
+    date: "nov. 2024"
+  },
+  {
+    title: "TouristSpain",
+    description: "En este proyecto pretendo mostrar monumentos relevantes...",
+    tech: [
+      { name: "Angular", icon: "/icons/angular.png" },
+      { name: "Git", icon: "/icons/git.png" },
+      { name: "HTML5", icon: "/icons/html5.png" },
+      { name: "CSS", icon: "/icons/css.png" },
+      { name: "TypeScript", icon: "/icons/typescript.png" }
+    ],
+    image: "/logo.png",
+    repo: "https://github.com/tuusuario/portafolio",
+    demo: "https://tuusuario.github.io/portafolio",
+    date: "abr. 2024 - nov. 2024"
+  },
+  {
+    title: "La primitiva",
+    description: "En este proyecto encontrarás una web donde se genera un número aleatorio...",
+    tech: [
+      { name: "HTML5", icon: "/icons/html5.png" },
+      { name: "CSS", icon: "/icons/css.png" },
+      { name: "JavaScript", icon: "/icons/js.png" },
+      { name: "Git", icon: "/icons/git.png" }
+    ],
+    image: "/logo.png",
+    repo: "https://github.com/tuusuario/portafolio",
+    demo: "https://tuusuario.github.io/portafolio",
+    date: "jul. 2024"
+  },
+  {
+    title: "Calculadora",
+    description: "Este proyecto es una calculadora realizada con React + Vite",
+    tech: [
+      { name: "React", icon: "/icons/react.png" },
+      { name: "Git", icon: "/icons/git.png" },
+      { name: "HTML5", icon: "/icons/html5.png" },
+      { name: "CSS", icon: "/icons/css.png" }
+    ],
+    image: "/logo.png",
+    repo: "https://github.com/tuusuario/portafolio",
+    demo: "https://tuusuario.github.io/portafolio",
+    date: "jun. 2024"
+  },
+  {
+    title: "InfoPersonRol",
+    description: "Este proyecto muestra información de personajes de rol...",
+    tech: [
+      { name: "React", icon: "/icons/react.png" },
+      { name: "Git", icon: "/icons/git.png" },
+      { name: "HTML5", icon: "/icons/html5.png" },
+      { name: "CSS", icon: "/icons/css.png" }
+    ],
+    image: "/logo.png",
+    repo: "https://github.com/tuusuario/portafolio",
+    demo: "https://tuusuario.github.io/portafolio",
+    date: "jun. 2024"
+  },
+  {
+    title: "AdivinaLaPelicula",
+    description: "Web con juegos sobre películas...",
+    tech: [
+      { name: "Angular", icon: "/icons/angular.png" },
+      { name: "PhpMyAdmin", icon: "/icons/phpmyadmin.png" },
+      { name: "PHP", icon: "/icons/php.png" },
+      { name: "Git", icon: "/icons/git.png" },
+      { name: "HTML5", icon: "/icons/html5.png" },
+      { name: "CSS", icon: "/icons/css.png" },
+      { name: "MySQL", icon: "/icons/mysql.png" },
+      { name: "TypeScript", icon: "/icons/typescript.png" }
+    ],
+    image: "/logo.png",
+    repo: "https://github.com/tuusuario/portafolio",
+    demo: "https://tuusuario.github.io/portafolio",
+    date: "sept. 2023 - dic. 2023"
+  }
+];
+
